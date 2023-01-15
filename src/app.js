49473433995b8abd5e8a351718a068f6ca9b27e7
leonardoyusuke <you@example.comleoyuhirano@gmail.com>
2023-01-15 20:31:08 +0000

    post participants

--- src/app.js
+++ src/app.js
@@ -1,21 +1,76 @@
 import express  from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { MongoClient, MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
+import joi from "joi"
+import dayjs from 'dayjs';
 
-const MongoClient = new MongoClient()
-let db;
 
-MongoClient.connect().then(() => {
-    db = MongoClient.db("")
-}).catch((err) => {
-    console.log("erro")
-})
+const server = express()
+server.use(express.json())
+server.use(cors())
+const PORT = 5001
 
 dotenv.config()
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db;
 
-const PORT = 5001
+const userSchema = joi.object({
+  name: joi.string().required()
+})
+
+try {
+    await mongoClient.connect()
+    db = mongoClient.db()
+  } catch (error) {
+    console.log(error)
+  }
+
+server.post("/participants", async (request, response) => {   
+  const participant = request.body
+  const validation = userSchema.validate(participant, { abortEarly: false }) //validacao
+  const time = Date.now();
+   if(validation.error) {
+      const errors = validation.error.details.map((detail) => detail.message);
+      return response.sendStatus(422).send(errors)}
+
+  try {
+    const nameInUse = await db.collection("participants").findOne(participant) //procurar nome igual
+    if(nameInUse){
+      return response.sendStatus(409)
+    }
+    await db.collection("participants").insertOne({...participant, lastStatus: time })
+    await db.collection("messages").insertOne({
+      from:participant.name,
+      to: "todos",
+      text: "entrou na sala...",
+      type: "status",
+      time: dayjs(Date.now()).format("HH:mm:ss")
+    })
+    return response.sendStatus(201)
+    console.log("deu")
+    }
+  catch (error) {
+    response.send(error)
+    console.log(error)
+  }
+  })
+
+server.get("/participants", async (request, response) => {
+    try { 
+        const participants = await db.collection("participants") //.find().toArray()
+        
+    } catch (error) {
+        res.status(500).send("Deu um erro no servidor de banco de dados")
+
+    }   
+})
+
+server.get("/messages"), async (request, response) => {
+    
+}
+
+server.listen(PORT, () => {
+    console.log(`server on port ${PORT}`)
+  })
