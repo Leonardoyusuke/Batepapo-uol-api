59cb2792b1adbd4cbf399f1b0b0def1000e93e3b
leonardoyusuke <you@example.comleoyuhirano@gmail.com>
2023-01-16 17:44:43 +0000

    fiz

--- src/app.js
+++ src/app.js
@@ -27,10 +27,11 @@ const messageSchema = joi.object({
 })
 
 setTimeout(removeInative,15000)
+
 async function removeInative(){
 const users = await db.collection("participants").find().toArray()
  users.forEach((u) => {
-  if((date.now()-u.lastStatus) > 10000 ){
+  if((Date.now()-u.lastStatus) > 10000 ){
     db.collection("participants").deleteOne({name:u.name})
     db.collection("messages").insertOne({
       from:u.name,
